feca35ef61086f896df623c939593ffceb82a698
Robert WC Seifert <rseifert.phone@gmail.com>
2019-09-17 15:19:51 -0400

    Updated page with github link

--- src/App.js
+++ src/App.js
@@ -6,17 +6,16 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hey this is a quick test of github pages working with github actions.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/DarkGuardsman/react-github-action-test"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Github Link
         </a>
       </header>
     </div>
